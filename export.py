cea65ddf3d376ccf7902cc94c38ef984a113e9a9
Max Zalubovskiy <44732505+viplash4@users.noreply.github.com>
2020-05-03 18:23:16 +0300

    csv export

--- export.py
+++ export.py
@@ -8,7 +8,7 @@ cursor = connection.cursor()
 tables = ["states","full_city_address","city_info"]
 try:
     for i in tables:
-        with open(i + '.csv', 'w', newline='') as NewFile:
+        with open(i + '.csv', 'w', newline='') as file:
             cursor.execute("SELECT * FROM " + i)
             row = cursor.fetchone()
 
@@ -16,13 +16,14 @@ try:
             for k in cursor.description:
                 titles.append(k[0])
             print(titles)
-            csv_writer = csv.writer(NewFile, delimiter=',')
+            csv_writer = csv.writer(file, delimiter=',')
             csv_writer.writerow(titles)
 
             while row:
                 csv_writer.writerow(row)
                 row = cursor.fetchone()
 
-finally:
-    cursor.close()
-    connection.close()
+except:
+    print('error happened')
+cursor.close()
+connection.close()
